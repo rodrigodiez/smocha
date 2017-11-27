51e9ba9bc56b3b00498497e35f9ff3bd040b9f8d
Rodrigo Díez Villamuera <rodrigo@rodrigodiez.io>
2017-11-27 22:06:36 +0000

    Implemented some tests to make travis integration useful

--- types/test.go
+++ types/test.go
@@ -2,9 +2,11 @@ package types
 
 type Test struct {
 	Url    string
-	Should struct {
-		Contain         string
-		HaveStatus      int    `yaml:"have_status"`
-		MatchJsonSchema string `yaml:"match_json_schema"`
-	}
+	Should Should
+}
+
+type Should struct {
+	Contain         string
+	HaveStatus      int    `yaml:"have_status"`
+	MatchJsonSchema string `yaml:"match_json_schema"`
 }
